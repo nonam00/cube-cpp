635023cceb0705565e6a645820ec78f2d40a053f
nonam00 <raisky.ilya3@yandex.ru>
2024-04-26 19:09:32 +0300

    Fixed the set values of rotation angles

--- main.cpp
+++ main.cpp
@@ -38,10 +38,8 @@ void line(Screen& screen, float x1, float y1, float x2, float y2)
 
   for(float i = 0; i < length; i++)
   {
-    screen.pixel(
-                  x1 + std::cos(angle) * i,
-                  y1 + std::sin(angle) * i
-                );
+    screen.pixel(x1 + std::cos(angle) * i,
+        y1 + std::sin(angle) * i);
   }
 }
 
@@ -80,28 +78,29 @@ int main()
   };
 
   // Calculate centroid
-  vec3 c{0,0,0};
+  vec3 centroid{0,0,0};
   for(auto& p : points)
   {
-    c.x += p.x;
-    c.y += p.y;
-    c.z += p.z;
+    centroid.x += p.x;
+    centroid.y += p.y;
+    centroid.z += p.z;
   }
-  c.x /= points.size();
-  c.y /= points.size();
-  c.z /= points.size();
+  centroid.x /= points.size();
+  centroid.y /= points.size();
+  centroid.z /= points.size();
   
   while(true)
   {
     for(auto& p : points)
     {
-      p.x -= c.x;
-      p.y -= c.y;
-      p.z -= c.z;
-      rotate(p, 0.02, 0.01, 0.04);
-      p.x += c.x;
-      p.y += c.y;
-      p.z += c.z;
+      p.x -= centroid.x;
+      p.y -= centroid.y;
+      p.z -= centroid.z;
+      rotate(p, 0.002, 0.001, 0.004);
+      p.x += centroid.x;
+      p.y += centroid.y;
+      p.z += centroid.z;
+      
       screen.pixel(p.x,p.y);
     }
 
@@ -116,7 +115,6 @@ int main()
     screen.show();
     screen.clear();
     screen.input();
-
     SDL_Delay(20);
   }
 
